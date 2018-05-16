f5ced2167824de5aabdce8a004e84095cf032be0
Jellificorn <jenny_wickman@hotmail.se>
2018-05-16 14:02:30 +0200

    Ändring av text i OmOss

--- OmOss.php
+++ OmOss.php
@@ -14,9 +14,9 @@ include "db_connect.php";
                 <h2>Loggen som ger dig överblick över ditt mående</h2>
 
                 <div id="text">
-                    <p>Ibland kan det vara intressant att inte bara reflektera över hur du mått men även att kunna beskriva i detalj för andra hur ditt mående varit. 
-                    Men dagarna flyter lätt ihop och man glömmer enkelt. Därför har vi skapat en tjänst där du enkelt kan kartlägga du ditt välmående från dag till dag. Inte bara efter ditt humör men 
-                    även fysiska påfrestningar du upplever och övriga kommentarer du har om dagen. Detta resulterar sedan i en graf som på ett enkelt visuellt sätt ger dig överblick över ditt egna måendes potentiella mönster. Börja logga ditt mående idag!</p>
+                    <p>Vill du bli lite mer medveten om hur ditt mående förändras över tid? Eller vill du ha ett hjälpmedel för att i detalj kunna beskriva ditt mående för andra? Då är Mood log för dig!
+                    Dagarna flyter lätt ihop och man glömmer enkelt hur man mått i samband med specifika dagar. Därför skapade vi Mood log där du enkelt kan kartlägga ditt välmående från dag till dag. Inte bara efter ditt humör men 
+                    även fysiska påfrestningar du upplever och övriga kommentarer du har om dagen. Detta resulterar sedan i en graf som på ett enkelt visuellt sätt ger dig överblick över ditt välmående. För att få en större förståelse för ditt mående, börja logga idag!</p>
                 </div>
 
     </body>
