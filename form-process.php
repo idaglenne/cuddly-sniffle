43ff7fcce7a321b373fd69ca025f40d6cfd4b5dc
evesandstrom <38068300+evesandstrom@users.noreply.github.com>
2018-05-23 13:26:16 +0200

    Löst clientID hittas i form

--- form-process.php
+++ form-process.php
@@ -5,21 +5,22 @@
     if (isset($_SESSION["clientID"]))
     {
         $client_ID =  $_SESSION["clientID"];
+        $moodComment = $connection->real_escape_string($_POST["commentSection"]);
+        $moodRating = $connection->real_escape_string($_POST["moodOneToSeven"]);
+        $moodSymptom = implode(',', $_POST["symptom"]);
+
+        $query = "INSERT INTO Log (clientID, rating, comment) VALUES ('".$client_ID."', '".$moodRating."', '".$moodComment."')";
+        $secondquery = "INSERT INTO Symptoms (symptomType) VALUES ('".$moodSymptom."')";
+        $connection->query($query);
+        $connection->query($secondquery);
+
+        header("Refresh: 0; URL=inloggad.php");
         
     }
-    /* else
+    else
     {
         header("Refresh: 0; URL=index.php");
         exit;
-    } */
+    }
 
-    $client_ID =  $_SESSION["clientID"];
-    $moodComment = $connection->real_escape_string($_POST["commentSection"]);
-    $moodRating = $connection->real_escape_string($_POST["moodOneToSeven"]);
-    $moodSymptom = $connection->real_escape_string($_POST["symptom"]);
-
-    $query = "INSERT INTO Log (clientID, rating, comment) VALUES ('".$client_ID."', '".$moodRating."', '".$moodComment."')";
-    $secondquery = "INSERT INTO Symptoms (symptomType) VALUES ('".$moodSymptom."')";
-    $connection->query($query);
-    $connection->query($secondquery);
 ?>
