75bc52b26b9a185a11ba874365312e35de1b5afa
evesandstrom <38068300+evesandstrom@users.noreply.github.com>
2018-05-23 12:53:27 +0200

    ?

--- form-process.php
+++ form-process.php
@@ -2,11 +2,23 @@
     include "db_connect.php";
     include "login_process.php";
 
+    if (isset($_SESSION["clientID"]))
+    {
+        $client_ID =  $_SESSION["clientID"];
+        
+    }
+    /* else
+    {
+        header("Refresh: 0; URL=index.php");
+        exit;
+    } */
+
     $moodComment = $connection->real_escape_string($_POST["commentSection"]);
     $moodRating = $connection->real_escape_string($_POST["moodOneToSeven"]);
     $moodSymptom = $connection->real_escape_string($_POST["symptom"]);
 
-    $query = "INSERT INTO Log (rating, comment) VALUES ('".$moodRating."', '".$moodComment."')";
+
+    
     $secondquery = "INSERT INTO Symptoms (symptomType) VALUES ('".$moodSymptom."')";
     $connection->query($query);
     $connection->query($secondquery);
