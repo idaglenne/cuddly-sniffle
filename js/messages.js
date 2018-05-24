1e4a2b10a285c2459fc6eaa4f06b61dc66c6298b
magdalena <karinmagdalenasorlle@gmail.com>
2018-05-24 11:01:01 +0200

    js

--- js/messages.js
+++ js/messages.js
@@ -1,21 +1,32 @@
-
-
-
-
 // dagens datum
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
+//var today = new Date();
+//var dd = today.getDate();
+//var mm = today.getMonth()+1; //January is 0!
+//var yyyy = today.getFullYear();
+//today = dd + '/' + mm + '/' + yyyy;
+//document.write(today);
 
 
-if(dd<10) {
-    dd = '0'+dd
+var day = new Date();
+var dayNumber = day.getDay();
+
+var moodRating; //hämtas??
+
+
+if (dayNumber == 4) {
+    if (moodRating < 10){
+        //kod för meddelande 1
+    }
+    if ((moodRating >= 11) || (moodRating =< 20))
+    {
+        //kod för meddelande 2
+    }
+    if ((moodRating >= 21) || (moodRating =< 30))
+    {
+        //kod för meddelande 3
+    }
+    if (moodRating > 30)
+    {
+        //kod för meddelande 4
+    }
 } 
-
-if(mm<10) {
-    mm = '0'+mm
-} 
-
-today = dd + '/' + mm + '/' + yyyy;
-document.write(today);
--- logdata.php
+++ logdata.php
@@ -4,9 +4,8 @@ include "login_process.php";
 
 header('Content-Type: application/json');
 
-
 $client_ID =  $_SESSION["clientID"];
-$date_query = "SELECT logDate, rating FROM Log WHERE clientID = '".$client_ID."'";
+$date_query = "SELECT logDate, rating FROM Log LIMIT 3";
 //$rating_query = "SELECT rating FROM Log WHERE clientID LIKE $client_ID";
 $dates = $connection->query($date_query);
 //$rating = $connection->query($rating_query);
@@ -18,13 +17,13 @@ foreach($dates as $row){
 
     }
 
-
-while ($weekrating = $dates->fetch_assoc()){
-
-    $weekrating = ($weekrating + $dates["rating"]);
-}
-
 print json_encode($data);
+//WHERE clientID = '".$client_ID."'
+
+
+$rating_query = "SELECT rating FROM Log WHERE clientID = $client_ID";
+
+
 
 
 
