acf0635bf6364318863bb05ef028d3cd2e22141f
Ida G <idaglenne@msn.com>
2018-05-24 15:10:28 +0200

    vet ej

--- js/chart.js
+++ js/chart.js
@@ -48,8 +48,9 @@ $(document).ready(function(){
                         data: mood.rating,
                         backgroundcolor: "#F8B195",
                         borderColor: "#F8B195",
-                        fill: false,
-                        lineTension: 0.5,
+                        fillColor: "#F8B195",
+                        fill: true,
+                        lineTension: 0.4,
                         pointRadius: 3
 
                     }
