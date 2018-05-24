b0a8a4429704359785c6ad23156dcd1aa0124ea8
Ida G <idaglenne@msn.com>
2018-05-24 10:51:34 +0200

    jj

--- logdata.php
+++ logdata.php
@@ -4,6 +4,7 @@ include "login_process.php";
 
 header('Content-Type: application/json');
 
+
 $client_ID =  $_SESSION["clientID"];
 $date_query = "SELECT logDate, rating FROM Log WHERE clientID = '".$client_ID."'";
 //$rating_query = "SELECT rating FROM Log WHERE clientID LIKE $client_ID";
