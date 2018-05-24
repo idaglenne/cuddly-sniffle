dbf154e3b3c1da82511baa0c777b1b763c2c8e57
Ida G <idaglenne@msn.com>
2018-05-24 11:11:45 +0200

    Revert "js"
    This reverts commit 1e4a2b10a285c2459fc6eaa4f06b61dc66c6298b.

--- js/messages.js
+++ js/messages.js
@@ -1,32 +1,21 @@
+
+
+
+
 // dagens datum
-//var today = new Date();
-//var dd = today.getDate();
-//var mm = today.getMonth()+1; //January is 0!
-//var yyyy = today.getFullYear();
-//today = dd + '/' + mm + '/' + yyyy;
-//document.write(today);
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
 
 
-var day = new Date();
-var dayNumber = day.getDay();
-
-var moodRating; //hämtas??
-
-
-if (dayNumber == 4) {
-    if (moodRating < 10){
-        //kod för meddelande 1
-    }
-    if ((moodRating >= 11) || (moodRating =< 20))
-    {
-        //kod för meddelande 2
-    }
-    if ((moodRating >= 21) || (moodRating =< 30))
-    {
-        //kod för meddelande 3
-    }
-    if (moodRating > 30)
-    {
-        //kod för meddelande 4
-    }
+if(dd<10) {
+    dd = '0'+dd
 } 
+
+if(mm<10) {
+    mm = '0'+mm
+} 
+
+today = dd + '/' + mm + '/' + yyyy;
+document.write(today);
