6f69cc9412bd3f2bf371217255a17288112bee9a
Jellificorn <jenny_wickman@hotmail.se>
2018-05-25 18:27:54 +0200

    WIEEE

--- OmOss.php
+++ OmOss.php
@@ -7,21 +7,20 @@ include "db_connect.php";
     <link rel="stylesheet" href="./design.css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <link rel="icon" type="image/png" href="./cloud.png">
+         <script src="js/returnButton.js"></script>
     </head>
     <body>
 
         <h1>om mood log</h1>
                 <h2>Loggen som ger dig överblick över ditt mående</h2>
-
-                <form action:"./inloggad.php">
-                <input type="submit" class="submitbutton" value="Tillbaka" name="submitbutton">
+        <button class="submitbutton" onclick="goBack()">Tillbaka</button>
+               
 
                 <div id="text">
                     <p>Vill du bli lite mer medveten om hur ditt mående förändras över tid? Eller vill du ha ett hjälpmedel för att i detalj kunna beskriva ditt mående för andra? Då är Mood log för dig!</p>
                     <p>Dagarna flyter lätt ihop och man glömmer enkelt hur man mått i samband med specifika dagar. Därför skapade vi Mood log där du enkelt kan kartlägga ditt välmående från dag till dag. Inte bara efter ditt humör men även efter fysiska påfrestningar du upplever och övriga kommentarer du har om dagen. Detta resulterar sedan i en graf som på ett enkelt visuellt sätt ger dig överblick över ditt välmående.</p><p>För att få en större förståelse för ditt mående, börja logga idag!</p>
                 </div>
         
-        
- </form>
+
     </body>
 </html>
