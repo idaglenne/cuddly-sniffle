fb854a4adb6a342b09ebe6f878e963e00bb3d87f
magdalena <karinmagdalenasorlle@gmail.com>
2018-05-28 15:46:02 +0200

    tog bort kommentarer

--- logdata.php
+++ logdata.php
@@ -7,10 +7,7 @@ header('Content-Type: application/json');
 
 $client_ID =  $_SESSION["clientID"];
 $date_query = "SELECT logDate, rating FROM Log WHERE clientID = '".$client_ID."'";
-
-//$rating_query = "SELECT rating FROM Log WHERE clientID LIKE $client_ID";
 $dates = $connection->query($date_query);
-//$rating = $connection->query($rating_query);
 
 $data = array();
 foreach($dates as $row){
