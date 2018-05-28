522cb14252ba2ffab2cc382b8eaf26ac77937f8d
magdalena <karinmagdalenasorlle@gmail.com>
2018-05-28 11:40:50 +0200

    nu så

--- inloggad.php
+++ inloggad.php
@@ -193,15 +193,9 @@ else{
       </header>
       <div class="w3-container">
         <?php
-                
-<<<<<<< HEAD
-                 
+                                 
                 if ($todays_log["rating"] == 1) {
                 echo "<p id='modalText'>"."Kanske känner du någon som mådde väldigt dåligt under en period men som nu mår bättre? Kolla fliken för kontakter och hitta den som passar dig bäst.". "</p>";
-=======
-                if ($todays_log["rating"] < 2) {
-                echo "<div class='modalText'>"."Kanske känner du någon som mådde väldigt dåligt under en period men som nu mår bättre? Kolla fliken för kontakter och hitta den som passar dig bäst.". "</div>";
->>>>>>> 4a33701a6fb9bfc674913f9a68d6108e5c563b89
                 }
                 if (($todays_log["rating"] == 2) || ($todays_log["rating"] == 3)) {
                 echo "<p class='modalText'>"."Känner du dig nere? Du vet väl att du kan vända dig till dessa stödkontakter om du behöver prata med någon.". "</p>";
@@ -209,13 +203,8 @@ else{
                 if (($todays_log["rating"] == 4) || ($todays_log["rating"] == 5)) {
                 echo "<p class='modalText'>"."Det verkar som att du mår ganska bra. Vad härligt! Har du gjort något särskilt under de senaste dagarna som påverkat dig positivt?"."</p>";
                 }
-<<<<<<< HEAD
                 if (($todays_log["rating"] == 6) || ($todays_log["rating"] == 7)) {
                 echo "<p id='modalText'>"."Vad roligt att du skattar ditt mående högt idag! Försök att stanna upp och känna efter hur det känns.". "</p>";
-=======
-                if ($todays_log["rating"] > 5) {
-                echo "<p class='modalText'>"."Vad roligt att du skattar ditt mående högt idag! Försök att stanna upp och känna efter hur det känns.". "</p>";
->>>>>>> 4a33701a6fb9bfc674913f9a68d6108e5c563b89
                 }
                   
         ?>
